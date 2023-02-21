8df187c65078b3cb9503b1a92058347fdbd54223
Riversleaf <sergo.joeleht@tktk.ee>
2023-02-22 00:00:25 +0200

    add week2 code

---
+++ OROperator/Program.cs
@@ -0,0 +1,23 @@
+﻿//Math >= 90 OR Chemistry >= 90
+
+//Math && Chemistry || Math && Biology || Chemistry && Biology
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter Your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Your Chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if((math >= 90 && chemistry >= 90) || (math >= 90 && biology >= 90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congratulations! You got accepted!");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
